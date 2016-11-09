9ec4ddb1757dc1e7f7867ea73aad6b11d329f6e3
rkt1661 <rkt1661@gmail.com>
2016-11-08 23:14:58 -0500

    Update Exercise2.java

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -9,21 +9,99 @@ TODO make sure we have no-op implementations of both engines.
 */
 public class Exercise2 {
 
-	public static class Car {
-		
-		private GasEngine engine = new GasEngine();
-		
-		public Car() {
-		}
-		
-		public void moveForward() {
-			engine.spinWheels();
-		}
-	}
-	
-	public static class GasEngine {
-		public void spinWheels() {
-			// no-op for now
-		}
-	}
+
+public static interface Engine{
+
+public void spinWheels();
+
+}
+
+
+
+public static class Car {
+
+
+private Engine engine = null;
+
+
+public Car(Engine engine) {
+
+this.engine=engine;
+
+}
+
+
+public void moveForward() {
+
+this.engine.spinWheels();
+
+}
+
+}
+
+
+public static class GasEngine implements Engine{
+
+@Override
+
+public void spinWheels() {
+
+// no-op for now
+
+}
+
+}
+
+
+public static class ElectricEngine implements Engine{
+
+@Override
+
+public void spinWheels() {
+
+// no-op for now
+
+}
+
+}
+
+
+public static interface EngineInjector{
+
+
+public Car getCar();
+
+
+}
+
+
+public static class GasEngineInjector implements EngineInjector{
+
+
+@Override
+
+public Car getCar(){
+
+return new Car(new GasEngine());
+
+}
+
+
+
+}
+
+public static class ElectricEngineInjector implements EngineInjector{
+
+@Override
+
+public Car getCar(){
+
+return new Car(new ElectricEngine());
+
+}
+
+}
+
+
+
 }
